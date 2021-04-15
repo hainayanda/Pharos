44d68a129bddd16bd803a548fb1a0342b535053a
Nayanda Haberty <nayanda1@outlook.com>
2021-04-15 18:50:13 +0700

    Update Scripts

--- .github/scripts/test_lib.sh
+++ .github/scripts/test_lib.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace Example/Impose.xcworkspace \
-            -scheme Impose-Example \
+xcodebuild -workspace Example/Pharos.xcworkspace \
+            -scheme Pharos-Example \
             -destination platform=iOS\ Simulator,OS=14.3,name=iPhone\ 11 \
             clean test | xcpretty
