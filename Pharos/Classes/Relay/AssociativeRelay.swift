d6f21d958ba1719938b8a592a86fbd20e370c9e1
Nayanda Haberty <nayanda1@outlook.com>
2021-07-09 17:17:07 +0700

    Make some method open

--- Pharos/Classes/Relay/AssociativeRelay.swift
+++ Pharos/Classes/Relay/AssociativeRelay.swift
@@ -15,6 +15,46 @@ open class AssociativeBearerRelay<Value>: BearerRelay<Value>, AssociativeRelay {
         self.associatedObject = object
         super.init(currentValue: currentValue, consumer: consumer)
     }
+    
+    open override func relay(changes: Changes<Value>) -> Bool {
+        super.relay(changes: changes)
+    }
+    
+    open override func ignore(when ignoring: @escaping Ignorer) -> Self {
+        super.ignore(when: ignoring) as? Self ?? self
+    }
+    
+    @discardableResult
+    open override func multipleSetDelayed(by interval: TimeInterval) -> Self {
+        super.multipleSetDelayed(by: interval) as? Self ?? self
+    }
+    
+    @discardableResult
+    open override func observe(on dispatcher: DispatchQueue) -> Self {
+        super.observe(on: dispatcher) as? Self ?? self
+    }
+    
+    @discardableResult
+    open override func syncWhenInSameThread() -> Self {
+        super.syncWhenInSameThread() as? Self ?? self
+    }
+    
+    open override func invokeRelay() {
+        super.invokeRelay()
+    }
+    
+    open override func removeAllNextRelays() {
+        super.removeAllNextRelays()
+    }
+    
+    @discardableResult
+    open override func addNext<Relay: BaseRelay<Value>>(relay: Relay) -> Relay {
+        super.addNext(relay: relay)
+    }
+    
+    open override func discard() {
+        super.discard()
+    }
 }
 
 open class AssociativeValueRelay<Value>: ValueRelay<Value>, AssociativeRelay {
@@ -25,6 +65,46 @@ open class AssociativeValueRelay<Value>: ValueRelay<Value>, AssociativeRelay {
         self.associatedObject = object
         super.init(currentValue: currentValue)
     }
+    
+    open override func relay(changes: Changes<Value>) -> Bool {
+        super.relay(changes: changes)
+    }
+    
+    open override func ignore(when ignoring: @escaping Ignorer) -> Self {
+        super.ignore(when: ignoring) as? Self ?? self
+    }
+    
+    @discardableResult
+    open override func multipleSetDelayed(by interval: TimeInterval) -> Self {
+        super.multipleSetDelayed(by: interval) as? Self ?? self
+    }
+    
+    @discardableResult
+    open override func observe(on dispatcher: DispatchQueue) -> Self {
+        super.observe(on: dispatcher) as? Self ?? self
+    }
+    
+    @discardableResult
+    open override func syncWhenInSameThread() -> Self {
+        super.syncWhenInSameThread() as? Self ?? self
+    }
+    
+    open override func invokeRelay() {
+        super.invokeRelay()
+    }
+    
+    open override func removeAllNextRelays() {
+        super.removeAllNextRelays()
+    }
+    
+    @discardableResult
+    open override func addNext<Relay: BaseRelay<Value>>(relay: Relay) -> Relay {
+        super.addNext(relay: relay)
+    }
+    
+    open override func discard() {
+        super.discard()
+    }
 }
 
 open class AssociativeTwoWayRelay<Value>: TwoWayRelay<Value>, AssociativeRelay {
@@ -34,5 +114,45 @@ open class AssociativeTwoWayRelay<Value>: TwoWayRelay<Value>, AssociativeRelay {
     public init(_ object: AnyObject, currentValue: Value) {
         self.associatedObject = object
         super.init(currentValue: currentValue)
+    }
+    
+    open override func relay(changes: Changes<Value>) -> Bool {
+        super.relay(changes: changes)
+    }
+    
+    open override func ignore(when ignoring: @escaping Ignorer) -> Self {
+        super.ignore(when: ignoring) as? Self ?? self
+    }
+    
+    @discardableResult
+    open override func multipleSetDelayed(by interval: TimeInterval) -> Self {
+        super.multipleSetDelayed(by: interval) as? Self ?? self
+    }
+    
+    @discardableResult
+    open override func observe(on dispatcher: DispatchQueue) -> Self {
+        super.observe(on: dispatcher) as? Self ?? self
+    }
+    
+    @discardableResult
+    open override func syncWhenInSameThread() -> Self {
+        super.syncWhenInSameThread() as? Self ?? self
+    }
+    
+    open override func invokeRelay() {
+        super.invokeRelay()
+    }
+    
+    open override func removeAllNextRelays() {
+        super.removeAllNextRelays()
+    }
+    
+    @discardableResult
+    open override func addNext<Relay: BaseRelay<Value>>(relay: Relay) -> Relay {
+        super.addNext(relay: relay)
+    }
+    
+    open override func discard() {
+        super.discard()
     }
 }
