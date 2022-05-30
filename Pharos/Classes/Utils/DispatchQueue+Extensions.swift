e72619ace12f7b7ba67b7348ffd9f8b426f5f53d
Nayanda Haberty <hainayanda@outlook.com>
2022-05-30 12:37:50 +0700

    Add DispatchQueue extension for safe async

--- Pharos/Classes/Utils/DispatchQueue+Extensions.swift
+++ Pharos/Classes/Utils/DispatchQueue+Extensions.swift
@@ -12,24 +12,15 @@ import Foundation
 ///   - work: Task to run
 public func syncIfPossible(on queue: DispatchQueue? = nil, execute work: @escaping () -> Void) {
     let dispatchQueue = (queue ?? .current) ?? .main
-    guard dispatchQueue != .main else {
-        syncOnMainIfPossible(execute: work)
-        return
-    }
-    guard DispatchQueue.current != queue else {
-        work()
-        return
-    }
-    queue?.async(execute: work)
+    dispatchQueue.asyncIfInDifferentQueue(execute: work)
 }
 
 public func syncOnMainIfPossible(execute work: @escaping () -> Void) {
-    if Thread.isMainThread
-        || DispatchQueue.current == .main {
+    if Thread.isMainThread {
         work()
         return
     }
-    DispatchQueue.main.async(execute: work)
+    DispatchQueue.main.asyncIfInDifferentQueue(execute: work)
 }
 
 extension DispatchQueue {
@@ -70,5 +61,42 @@ public extension DispatchQueue {
     static var currentQueueLabel: String? { current?.label }
     static var current: DispatchQueue? {
         getSpecific(key: key)?.queue ?? OperationQueue.current?.underlyingQueue
+    }
+}
+
+public extension DispatchQueue {
+    func safeSync(execute workItem: DispatchWorkItem) {
+        DispatchQueue.registerDetection(of: self)
+        guard DispatchQueue.current != self else {
+            workItem.perform()
+            return
+        }
+        sync(execute: workItem)
+    }
+    
+    func safeSync<Return>(execute block: () -> Return) -> Return {
+        DispatchQueue.registerDetection(of: self)
+        guard DispatchQueue.current != self else {
+            return block()
+        }
+        return sync(execute: block)
+    }
+    
+    func asyncIfInDifferentQueue(execute workItem: DispatchWorkItem) {
+        DispatchQueue.registerDetection(of: self)
+        guard DispatchQueue.current != self else {
+            workItem.perform()
+            return
+        }
+        async(execute: workItem)
+    }
+    
+    func asyncIfInDifferentQueue(execute block: @escaping () -> Void) {
+        DispatchQueue.registerDetection(of: self)
+        guard DispatchQueue.current != self else {
+            block()
+            return
+        }
+        async(execute: block)
     }
 }
